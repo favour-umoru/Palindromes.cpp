365c428ea93f793c59283deab84d3f6a9b30396b
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-22 15:28:27 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include<cmath>
+#include<cstdlib>
+#include "functions.h"
+
+using namespace std;
