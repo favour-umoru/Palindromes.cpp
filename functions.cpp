0fffd5b0c7c851f0519e38635d5401be818cac99
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-22 21:36:17 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -16,4 +16,30 @@ void printUsageInfo(string nameExecutable)
 bool isPalindrome(string nameExecutable)
 {
   
+  return bool;
 }
+
+
+bool isPalindromeR(string nameExecutable)
+{
+  
+  return bool;
+}
+
+string preprocessString(string nameExecutable)
+{
+  string returnString;
+  char lowerConvert;
+  int i = 0;
+  
+  while (nameExecutable[i])
+  {
+    lowerConvert=nameExecutable[i];
+    putchar (tolower(lowerConvert));
+    i++;
+  }
+  
+  
+  return returnString;
+}
+
