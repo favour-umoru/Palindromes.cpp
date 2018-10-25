79b9234c0d986b7af1da48015b6a30a7348f201b
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-25 12:05:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,16 +12,16 @@ using namespace std;
 int main(int argc, char* argv[]) {
   
   // Get input from the user using the command line which has been declared above
-  string getUserString;
-  
   // Get the flags form the user which will determine how the palindrome will be evaluated.
-  string flag;
-  
   // Assign the string gotton from the command line as the input from the user
-  getUserString = argv[1];
-  flag = argv[2];
+  // If there is only the string passed with no additional flags, do the basics of the program
+  // Display the Input into the command line.
+  // Add the flags needed to assess if the string is a palindrome.
+ 
+  // Call the first function to display the input into the command line
+  printUsageInfo(argc, argv);
   
-  
+  // Call the function to determine if it is a palindrome.
   
   return 0;
 }
